d45b78fd79633864b6f37b8b02c153beb21554c5
HappyFox <keith.baston@gmail.com>
2012-08-02 20:17:21 -0700

    Adding a setup.py. This currently seems not to work on Angstrom linux. Python complains about missing ssh modules. Still seems to copy the file to correct dir however.

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+from distutils.core import setup
+
+setup(name='i2c',
+      version='0.1',
+      description='A simple i2c interface for embedded unixes',
+      author='Keith Baston',
+      author_email='keith.baston@gmail.com',
+      url='https://github.com/HappyFox/Python-i2c',
+      packages=['i2c'],
+     )
